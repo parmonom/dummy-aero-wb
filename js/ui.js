f6069e9a8fd52f4325447f584ef5334d31dcbc2f
john <>
2021-04-21 13:49:03 +0200

    Implement fuel and baggage limits with 2 occupants

--- js/ui.js
+++ js/ui.js
@@ -13,7 +13,7 @@ var baggageWeightDefault = 0;
 
 var paxMaxWeight = 200;
 var occupantMaxWeight = 110;
-
+var twoOccupantWeight = 25;
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 // GET ID
@@ -35,93 +35,9 @@ var sliderBaggage = document.getElementById('slider-baggage');
 var switchPosition = 1;
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- 
-// EMPTY WEIGHT
-// Synchro with the input
-inputEmptyWeight.addEventListener('change', function () {
-    // sliderFuel.noUiSlider.set([this.value, null]);
-    updateFigure();
-});
+// CREATE SLIDERS
 
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- 
-// START MOMENT
-// Synchro with the input
-inputStartMoment.addEventListener('change', function () {
-    // sliderFuel.noUiSlider.set([this.value, null]);
-    updateFigure();
-});
-
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- 
-// FUEL
-// Init slider
-noUiSlider.create(sliderFuel, {
-    start: [fuelQuantityDefault],
-    connect: true,
-    behaviour: 'snap',
-    step: 1,
-    range: {
-        'min': 0,
-        'max': fuelQuantityDefault
-    },
-    pips: {
-        // mode: 'range',
-        mode: 'values',
-        values: [0, 30, 60, 90, 120, 150],
-        density: 7,
-    },
-});
-
-// When slider is update
-sliderFuel.noUiSlider.on('update', function (values, handle) {
-    var value = values[handle];
-    inputFuel.value = Math.round(value);
-    updateFigure();
-});
-
-// Synchro with the input
-inputFuel.addEventListener('change', function () {
-    sliderFuel.noUiSlider.set([this.value, null]);
-});
-
-
-
-
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- 
-// PASSENGER
-// Init slider
-noUiSlider.create(sliderPassenger, {
-    start: [passengerWeightDefault],
-    connect: true,
-    behaviour: 'snap',
-    step: 1,
-    range: {
-        'min': 0,
-        'max': occupantMaxWeight
-    },
-    pips: {
-        // mode: 'range',
-        mode: 'values',
-        values: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
-        density: 5,
-    },
-});
-
-// When slider is update
-sliderPassenger.noUiSlider.on('update', function (values, handle) {
-    // console.log('passenger update')
-    var value = values[handle];
-    inputPassenger.value = Math.round(value);
-    // crossUpdateSlider(sliderPilot)
-    updateFigure()
-});
-
-// Synchro with the input
-inputPassenger.addEventListener('change', function () {
-    sliderPassenger.noUiSlider.set([this.value, null]);
-});
-
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 // PILOT 
-// Init slider
 noUiSlider.create(sliderPilot, {
     start: [pilotWeightDefault],
     connect: true,
@@ -141,28 +57,25 @@ noUiSlider.create(sliderPilot, {
     },
 });
 
-// When slider is update
-sliderPilot.noUiSlider.on('update', function (values, handle) {
-    var value = values[handle];
-    inputPilot.value = Math.round(value);  
-    var pad = Math.max(0, occupantMaxWeight - (paxMaxWeight - value));
-
-    // console.log('pad', pad)
-    sliderPassenger.noUiSlider.updateOptions({
-        padding: [0, pad]
-    })
-    
-    updateFigure()
+// PASSENGER
+noUiSlider.create(sliderPassenger, {
+    start: [passengerWeightDefault],
+    connect: true,
+    behaviour: 'snap',
+    step: 1,
+    range: {
+        'min': 0,
+        'max': occupantMaxWeight
+    },
+    pips: {
+        // mode: 'range',
+        mode: 'values',
+        values: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
+        density: 5,
+    },
 });
 
-// Synchro with the input
-inputPilot.addEventListener('change', function () {
-    sliderPilot.noUiSlider.set([this.value, null]);
-});
-
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 // BAGGAGE
-// Init slider
 noUiSlider.create(sliderBaggage, {
     start: [baggageWeightDefault],
     connect: true,
@@ -180,17 +93,114 @@ noUiSlider.create(sliderBaggage, {
     },
 });
 
-// When slider is update
+// FUEL
+noUiSlider.create(sliderFuel, {
+    start: [fuelQuantityDefault],
+    connect: true,
+    behaviour: 'snap',
+    step: 1,
+    range: {
+        'min': 0,
+        'max': fuelQuantityDefault
+    },
+    pips: {
+        // mode: 'range',
+        mode: 'values',
+        values: [0, 30, 60, 90, 120, 150],
+        density: 7,
+    },
+});
+
+
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+// ON UPDATE SLIDERS
+// PILOT
+sliderPilot.noUiSlider.on('update', function (values, handle) {
+    var value = values[handle];
+    inputPilot.value = Math.round(value);  
+    // passenger max weight admissible
+    var pad = Math.max(0, occupantMaxWeight - (paxMaxWeight - value));
+
+    sliderPassenger.noUiSlider.updateOptions({
+        padding: [0, pad]
+    })
+    updateFigure()
+});
+
+// PASSENGER 
+sliderPassenger.noUiSlider.on('update', function (values, handle) {
+    var value = values[handle];
+    inputPassenger.value = Math.round(value);
+    // reduce fuel and baggage admissible with two occupants
+    if (value > twoOccupantWeight) {
+        sliderFuel.noUiSlider.updateOptions({
+            padding: [0, 50]
+        })
+        sliderBaggage.noUiSlider.updateOptions({
+            padding: [0, 10]
+        })
+    } else {
+        sliderFuel.noUiSlider.updateOptions({
+            padding: [0, 0]
+        })
+        sliderBaggage.noUiSlider.updateOptions({
+            padding: [0, 0]
+        })
+    }
+    updateFigure()
+});
+
+// BAGGAGE
 sliderBaggage.noUiSlider.on('update', function (values, handle) {
     var value = values[handle];
     inputBaggage.value = Math.round(value);
     updateFigure()
 });
 
-// Synchro with the input
+// FUEL
+sliderFuel.noUiSlider.on('update', function (values, handle) {
+    var value = values[handle];
+    inputFuel.value = Math.round(value);
+    updateFigure();
+});
+
+
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+// ON UPDATE INPUTS
+// EMPTY WEIGHT
+inputEmptyWeight.addEventListener('change', function () {
+    // sliderFuel.noUiSlider.set([this.value, null]);
+    updateFigure();
+});
+
+// START MOMENT
+inputStartMoment.addEventListener('change', function () {
+    // sliderFuel.noUiSlider.set([this.value, null]);
+    updateFigure();
+});
+
+// PILOT
+inputPilot.addEventListener('change', function () {
+    sliderPilot.noUiSlider.set([this.value, null]);
+});
+
+// PASSENGER
+inputPassenger.addEventListener('change', function () {
+    sliderPassenger.noUiSlider.set([this.value, null]);
+});
+
+// BAGGAGE
 inputBaggage.addEventListener('change', function () {
     sliderBaggage.noUiSlider.set([this.value, null]);
 });
+
+// FUEL 
+inputFuel.addEventListener('change', function () {
+    sliderFuel.noUiSlider.set([this.value, null]);
+});
+
+
+
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 // SWITCH BALLAST
@@ -209,10 +219,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
-// function updateInputs() {
-//     if (hour < 18) {
-//         greeting = "Good day";
-//       } 
-
-// }
