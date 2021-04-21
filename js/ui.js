2ab2ff1d8dc97f29a94a861ecf40f8a4b2eeceb1
john <>
2021-04-21 11:52:17 +0200

    Implement pax max weight dynamicaly

--- js/ui.js
+++ js/ui.js
@@ -11,6 +11,9 @@ var pilotWeightDefault = 75;
 var passengerWeightDefault = 0;
 var baggageWeightDefault = 0;
 
+var paxMaxWeight = 200;
+var occupantMaxWeight = 110;
+
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 // GET ID
@@ -29,12 +32,12 @@ var sliderPassenger = document.getElementById('slider-passenger');
 var sliderBaggage = document.getElementById('slider-baggage');
 
 // switch
-var switchPosition = 1; 
+var switchPosition = 1;
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 // EMPTY WEIGHT
 // Synchro with the input
-inputEmptyWeight.addEventListener('change', function() {
+inputEmptyWeight.addEventListener('change', function () {
     // sliderFuel.noUiSlider.set([this.value, null]);
     updateFigure();
 });
@@ -42,7 +45,7 @@ inputEmptyWeight.addEventListener('change', function() {
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- 
 // START MOMENT
 // Synchro with the input
-inputStartMoment.addEventListener('change', function() {
+inputStartMoment.addEventListener('change', function () {
     // sliderFuel.noUiSlider.set([this.value, null]);
     updateFigure();
 });
@@ -68,51 +71,18 @@ noUiSlider.create(sliderFuel, {
 });
 
 // When slider is update
-sliderFuel.noUiSlider.on('update', function(values, handle) {
+sliderFuel.noUiSlider.on('update', function (values, handle) {
     var value = values[handle];
     inputFuel.value = Math.round(value);
     updateFigure();
 });
 
 // Synchro with the input
-inputFuel.addEventListener('change', function() {
+inputFuel.addEventListener('change', function () {
     sliderFuel.noUiSlider.set([this.value, null]);
 });
 
 
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- 
-// PILOT 
-// Init slider
-noUiSlider.create(sliderPilot, {
-    start: [pilotWeightDefault],
-    connect: true,
-    behaviour: 'snap',
-    // step: 1,
-    padding: [55],
-    range: {
-        'min': 0,
-        'max': 110
-    },
-
-    pips: {
-        // mode: 'range',
-        mode: 'values',
-        values: [60, 70, 80, 90, 100, 110],
-        density: 5,
-    },
-});
-
-// When slider is update
-sliderPilot.noUiSlider.on('update', function(values, handle) {
-    var value = values[handle];
-    inputPilot.value = Math.round(value);
-    updateFigure()
-});
-
-// Synchro with the input
-inputPilot.addEventListener('change', function() {
-    sliderPilot.noUiSlider.set([this.value, null]);
-});
 
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- 
@@ -125,7 +95,7 @@ noUiSlider.create(sliderPassenger, {
     step: 1,
     range: {
         'min': 0,
-        'max': 110
+        'max': occupantMaxWeight
     },
     pips: {
         // mode: 'range',
@@ -136,15 +106,58 @@ noUiSlider.create(sliderPassenger, {
 });
 
 // When slider is update
-sliderPassenger.noUiSlider.on('update', function(values, handle) {
+sliderPassenger.noUiSlider.on('update', function (values, handle) {
+    // console.log('passenger update')
     var value = values[handle];
     inputPassenger.value = Math.round(value);
+    // crossUpdateSlider(sliderPilot)
     updateFigure()
 });
 
 // Synchro with the input
-inputPassenger.addEventListener('change', function() {
+inputPassenger.addEventListener('change', function () {
     sliderPassenger.noUiSlider.set([this.value, null]);
+});
+
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- 
+// PILOT 
+// Init slider
+noUiSlider.create(sliderPilot, {
+    start: [pilotWeightDefault],
+    connect: true,
+    behaviour: 'snap',
+    step: 1,
+    padding: [55, 0],
+    range: {
+        'min': 0,
+        'max': occupantMaxWeight
+    },
+
+    pips: {
+        // mode: 'range',
+        mode: 'values',
+        values: [60, 70, 80, 90, 100, 110],
+        density: 5,
+    },
+});
+
+// When slider is update
+sliderPilot.noUiSlider.on('update', function (values, handle) {
+    var value = values[handle];
+    inputPilot.value = Math.round(value);  
+    var pad = Math.max(0, occupantMaxWeight - (paxMaxWeight - value));
+
+    // console.log('pad', pad)
+    sliderPassenger.noUiSlider.updateOptions({
+        padding: [0, pad]
+    })
+    
+    updateFigure()
+});
+
+// Synchro with the input
+inputPilot.addEventListener('change', function () {
+    sliderPilot.noUiSlider.set([this.value, null]);
 });
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- 
@@ -168,14 +181,14 @@ noUiSlider.create(sliderBaggage, {
 });
 
 // When slider is update
-sliderBaggage.noUiSlider.on('update', function(values, handle) {
+sliderBaggage.noUiSlider.on('update', function (values, handle) {
     var value = values[handle];
     inputBaggage.value = Math.round(value);
     updateFigure()
 });
 
 // Synchro with the input
-inputBaggage.addEventListener('change', function() {
+inputBaggage.addEventListener('change', function () {
     sliderBaggage.noUiSlider.set([this.value, null]);
 });
 
@@ -183,16 +196,23 @@ inputBaggage.addEventListener('change', function() {
 // SWITCH BALLAST
 document.addEventListener('DOMContentLoaded', function () {
     var checkbox = document.querySelector('input[type="checkbox"]');
-  
+
     checkbox.addEventListener('change', function () {
-      if (checkbox.checked) {
-        // console.log('Checked REAR');
-        switchPosition = 0;
-        updateFigure()
-      } else {
-        // console.log('Not checked FRONT');
-        switchPosition = 1;
-        updateFigure()
-      }
+        if (checkbox.checked) {
+            // console.log('Checked REAR');
+            switchPosition = 0;
+            updateFigure()
+        } else {
+            // console.log('Not checked FRONT');
+            switchPosition = 1;
+            updateFigure()
+        }
     });
-  });
+});
+
+// function updateInputs() {
+//     if (hour < 18) {
+//         greeting = "Good day";
+//       } 
+
+// }
